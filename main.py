1fb8362dd976c153be5c0a98fcafa7a702ea4e4a
AhmadKhawar <ahmadkhawar86@gmail.com>
2025-07-04 01:50:09 +0500

    Connected all modules in main.py to launch app from login screen

---
+++ main.py
@@ -0,0 +1,9 @@
+from login import LoginApp
+from db import create_tables
+import tkinter as tk
+
+if __name__ == "__main__":
+    create_tables()  # Ensure DB is ready
+    root = tk.Tk()
+    app = LoginApp(root)
+    root.mainloop(
